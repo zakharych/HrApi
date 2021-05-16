ca0f163157e4b7f05ac06632da90f633acef20af
zakharych <alexzlkl@yandex.ru>
2021-05-16 23:18:08 +0300

    feat: fix pk list styles

--- js/condition.js
+++ js/condition.js
@@ -1,34 +1,32 @@
+const openItem = (item) => {
+  const container = item.closest(".ticket");
+  const contentBlock = container.find(".condition__item-info");
+  const textBlock = contentBlock.find(".condition__item-titleblock");
+  const reqHeight = textBlock.height();
+  container.addClass("active");
+  contentBlock.height(reqHeight);
+  console.log(reqHeight);
+};
 
-const openItem = item => {
-    const container = item.closest('.ticket');
-    const contentBlock = container.find(".condition__item-info");
-    const textBlock = contentBlock.find(".condition__item-titleblock");
-    const reqHeight = textBlock.height();
-    container.addClass("active")
-    contentBlock.height(reqHeight);
-    console.log(reqHeight);
-}
+const closeEveryItem = (container) => {
+  const item = container.find(".condition__item-info");
+  const itemContainer = container.find(".ticket");
 
-const closeEveryItem = container => {
-    const item = container.find('.condition__item-info');
-    const itemContainer = container.find(".ticket");
+  itemContainer.removeClass("active");
+  item.height(0);
+};
 
-    itemContainer.removeClass("active");
-    item.height(0); 
-}
+$(".condition__button").click((e) => {
+  const $this = $(e.currentTarget);
+  const container = $this.closest(".tickets");
+  const elemContainer = $this.closest(".ticket");
 
-$('.condition__button').click(e => {
-    const $this = $(e.currentTarget);
-    const container = $this.closest('.tickets');
-    const elemContainer = $this.closest(".ticket");
-
-    if (elemContainer.hasClass("active")) {
-        closeEveryItem(container);
-    } else {
-        closeEveryItem(container);
-        openItem($this);   
-    }
+  if (elemContainer.hasClass("active")) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItem($this);
+  }
+});
 
 
-}
-);
