451105574550294d0fe22c254b30fa723f3105da
zakharych <alexzlk@yandex.ru>
2021-05-20 15:27:58 +0300

    feat: two speakers in conf

--- js/createTable.js
+++ js/createTable.js
@@ -1,14 +1,11 @@
 const table = document.querySelector(".table");
 (async () => {
   let DB = await (await fetch("./js/timetable.txt")).json();
-
   createTable(DB);
 })();
 
 function createTable(element) {
-  // console.log(element[`tableHeaders`]);
   element[`DayTable`].forEach((dayTable) => {
-    // console.log(dayTable);
     const tableInner = document.createElement("div");
     tableInner.className = "table__inner";
     table.append(tableInner);
@@ -18,7 +15,6 @@ function createTable(element) {
     tableHeader.innerText = `${dayTable.tableHeader}`;
     tableInner.append(tableHeader);
 
-    // console.log(dayTable.tableLines);
     dayTable.tableLines.forEach((tableLines) => {
       const tableInnerTable = document.createElement("div");
       tableInnerTable.className = "table__inner-table";
@@ -31,7 +27,6 @@ function createTable(element) {
       tableInnerTable.append(tableInnerHeader);
 
       tableLines.tableLideSchedule.forEach((slot) => {
-        // console.log(slot);
         const tableRow = document.createElement("div");
         tableRow.className = "table__row";
         tableInnerTable.append(tableRow);
@@ -42,42 +37,46 @@ function createTable(element) {
         tableRow.append(time);
 
         if (slot.theme) {
-          console.log(slot.theme);
           const theme = document.createElement("a");
           theme.className = "table__item table__theme";
 
           theme.innerText = `${slot.theme}`;
           if (slot.themeLink) {
-            theme.classList.add('table__link')
+            theme.classList.add("table__link");
             theme.href = `${slot.themeLink}`;
           }
           tableRow.append(theme);
+          const speakerContainer = document.createElement("div");
+          speakerContainer.className = "table__item table__speaker-container";
 
-          const speaker = document.createElement("div");
-          speaker.className = "table__item table__speaker";
-          tableRow.append(speaker);
+          tableRow.append(speakerContainer);
 
-          const speakerName = document.createElement("div");
-          speakerName.className = "table__speaker-name";
-          speakerName.innerText = `${slot.speaker["name"]}`;
-          speaker.append(speakerName);
+          slot.speaker.forEach((speaker) => {
+            const speakerItem = document.createElement("div");
+            speakerItem.className = "table__speaker";
+            speakerContainer.append(speakerItem);
 
-          const speakerLocation = document.createElement("div");
-          speakerLocation.className = "table__speaker-location";
-          speakerLocation.innerText = `${slot.speaker["location"]}`;
-          speaker.append(speakerLocation);
+            const speakerName = document.createElement("div");
+            speakerName.className = "table__speaker-name";
+            speakerName.innerText = `${speaker["name"]}`;
+            speakerItem.append(speakerName);
 
-          const speakerPosition = document.createElement("div");
-          speakerPosition.className = "table__speaker-position";
-          speakerPosition.innerText = `${slot.speaker["position"]}`;
-          speaker.append(speakerPosition);
+            const speakerLocation = document.createElement("div");
+            speakerLocation.className = "table__speaker-location";
+            speakerLocation.innerText = `${speaker["location"]}`;
+            speakerItem.append(speakerLocation);
 
-          const speakerCompany = document.createElement("div");
-          speakerCompany.className = "table__speaker-company";
-          speakerCompany.innerText = `${slot.speaker["company"]}`;
-          speaker.append(speakerCompany);
+            const speakerPosition = document.createElement("div");
+            speakerPosition.className = "table__speaker-position";
+            speakerPosition.innerText = `${speaker["position"]}`;
+            speakerItem.append(speakerPosition);
+
+            const speakerCompany = document.createElement("div");
+            speakerCompany.className = "table__speaker-company";
+            speakerCompany.innerText = `${speaker["company"]}`;
+            speakerItem.append(speakerCompany);
+          });
         } else {
-          console.log(slot.noSpeakerLine);
           const breaking = document.createElement("div");
           breaking.className = "table__item table__break";
           breaking.innerText = `${slot.noSpeakerLine}`;
@@ -86,78 +85,4 @@ function createTable(element) {
       });
     });
   });
-
-  // const speakerItem = document.createElement("li");
-  // speakerItem.className = "pk__item";
-  // speakersList.append(speakerItem);
-
-  // const speakerLink = document.createElement("a");
-  // speakerLink.className = "pk__link";
-  // speakerLink.href = element.url;
-  // speakerLink.target = "_blank";
-  // speakerItem.append(speakerLink);
-
-  // const speakerPic = document.createElement("div");
-  // speakerPic.className = "pk__img";
-  // speakerLink.append(speakerPic);
-
-  // const speakerImg = document.createElement("img");
-  // speakerImg.className = "pk__avatar";
-  // speakerImg.src = element.avatar;
-  // speakerPic.append(speakerImg);
-
-  // const speakerTitle = document.createElement("div");
-  // speakerTitle.className = "pk__title";
-  // speakerLink.append(speakerTitle);
-
-  // const speakersName = document.createElement("p");
-  // speakersName.className = "pk__name";
-  // const speakerNameStr = element.name.split(" ").join("\n");
-  // speakersName.textContent = speakerNameStr;
-  // speakerTitle.append(speakersName);
-
-  // const speakersSity = document.createElement("p");
-  // speakersSity.className = "pk__city";
-  // speakersSity.textContent = element.location;
-  // speakerTitle.append(speakersSity);
-
-  // const speakersPosition = document.createElement("p");
-  // speakersPosition.className = "pk__position";
-  // speakersPosition.textContent = element.position;
-  // speakerTitle.append(speakersPosition);
-
-  // const speakersCompany = document.createElement("p");
-  // speakersCompany.className = "pk__company";
-  // speakersCompany.textContent = element.company;
-  // speakerTitle.append(speakersCompany);
-}
-
-function resizeSlideHeight() {
-  const slider = document.querySelector(".speakers__list");
-  slides = slider.querySelectorAll(".slick-slide");
-  let maxHeight = 0;
-  slides.forEach((element) => {
-    const firstSlideOffset = element.querySelector(".pk__item").offsetHeight;
-    if (firstSlideOffset > maxHeight) {
-      maxHeight = firstSlideOffset;
-    }
-  });
-  slides.forEach((element) => {
-    element.querySelector(".pk__item").style.minHeight = `${maxHeight}px`;
-    if (element.lastChild.querySelector(".pk__link")) {
-      element.lastChild.querySelector(".pk__link").style.marginBottom = 0;
-    }
-  });
-}
-
-function runSpicersSlider() {
-  $(".speakers__list").slick({
-    infinite: false,
-    speed: 300,
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    rows: 2,
-    dots: false,
-  });
-  resizeSlideHeight();
 }
--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,64 @@
-function sliderWidthMonipulation() {
-  const list = document.querySelector(".history__list");
-  let innerWidth = 0;
-  let a = 0;
-  for (let i = 0; i < list.children.length; i++) {
-    innerWidth +=
-      list.children[i].offsetWidth +
-      Number(
-        getComputedStyle(list.children[i]).marginRight.slice(
-          0,
-          getComputedStyle(list.children[i]).marginRight.length - 2
-        )
-      );
-    a += list.children[i].offsetWidth;
-  }
-  if (innerWidth > list.offsetWidth) {
-    list.classList.add("history__list--width");
-  }
-}
+// function sliderWidthMonipulation() {
+//   const list = document.querySelector(".history__list");
+//   let innerWidth = 0;
+//   let a = 0;
+//   for (let i = 0; i < list.children.length; i++) {
+//     innerWidth +=
+//       list.children[i].offsetWidth +
+//       Number(
+//         getComputedStyle(list.children[i]).marginRight.slice(
+//           0,
+//           getComputedStyle(list.children[i]).marginRight.length - 2
+//         )
+//       );
+//     a += list.children[i].offsetWidth;
+//   }
+//   if (innerWidth > list.offsetWidth) {
+//     list.classList.add("history__list--width");
+//   }
+// }
 
-$(document).ready(function () {
-  const history__list = document.querySelector(".history__list");
-  sliderWidthMonipulation();
-  $(".history__list").slick({
-    infinite: false,
-    slidesToShow: 11,
-    slidesToScroll: 1,
-    draggable: false,
-  });
-});
+// $(document).ready(function () {
+//   const history__list = document.querySelector(".history__list");
+//   sliderWidthMonipulation();
+//   $(".history__list").slick({
+//     infinite: false,
+//     slidesToShow: 11,
+//     slidesToScroll: 1,
+//     draggable: false,
+//   });
+// });
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var balancedStringSplit = function(s) {
+  // time O(n)
+  // space O(n)
+
+// hint suggested use number to record how many L R
+// but I still think stack will be better to understand
+
+  let count = 0
+  const stack = [s[0]] // get first char into the stack as start
+
+  for (let i = 1; i < s.length; i++) {
+  // loop each char in the string, if its same with prev char in the stack or the stack is empty
+  // means we need to push into the stack and continue
+    if (s[i] === stack[stack.length - 1] || stack.length < 1) {
+      stack.push(s[i])
+      continue
+    }
+  // if it is not same e.g. L meet R or R meet L
+  // it means we hit a "couple", we remove the last char in the stack
+  // check if its empty after our removal, then it means we actually completed a minimum balanced "pair" "combo"
+  // thus, we add the count
+    stack.pop()
+    if (stack.length < 1) count += 1
+  }
+  return count
+};
+
+console.log(balancedStringSplit("RLRRRLLRLL"));
